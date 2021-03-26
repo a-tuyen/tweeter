d07335868582e80412731ad33d5500179a9f7d3c
Amanda Tuyen <atuyen26@gmail.com>
2021-03-26 09:14:01 +0000

    implement character counter and design for new tweet box

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function(event) {
+  $('#tweet-text').on('input', function() {
+    let charLeft = 140;
+    let textLength = $(this).val().length; //length of input in textbox
+    charLeft -= textLength;
+    let newDisplayNum = $('output').val(charLeft); //sets new value of char remaining
+
+    if (charLeft < 0) {
+      newDisplayNum.addClass("red-font");
+    } else {
+      newDisplayNum.removeClass("red-font");
+    }
+
+  })
+})
