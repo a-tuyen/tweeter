9b828c12429248e8f54630fb6b8d97eeedc1b1e0
Amanda Tuyen <atuyen26@gmail.com>
2021-03-27 01:07:51 +0000

    create style and hover effect  for posted tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,13 +5,13 @@ $(document).ready(function(event) {
     let charLeft = 140;
     let textLength = $(this).val().length; //length of input in textbox
     charLeft -= textLength;
-    let newDisplayNum = $('output').val(charLeft); //sets new value of char remaining
+    let newDisplayNum = $('output').val(charLeft); //sets new value of char remaining and assigns <output></output> to variable
 
     if (charLeft < 0) {
-      newDisplayNum.addClass("red-font");
+      newDisplayNum.addClass("red-font"); 
     } else {
       newDisplayNum.removeClass("red-font");
-    }
+    } 
 
   })
 })
